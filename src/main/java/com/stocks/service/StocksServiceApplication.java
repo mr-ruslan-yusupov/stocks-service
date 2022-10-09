ab0ee43e4d5cffb86a5e3e92963d2c6047ea05b0
mr-ruslan-yusupov <mr.ruslan.yusupov@gmail.com>
2022-10-09 18:17:09 +0300

    Initial commit

---
+++ src/main/java/com/stocks/service/StocksServiceApplication.java
@@ -0,0 +1,13 @@
+package com.stocks.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StocksServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StocksServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stocks/service/StocksServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stocks.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StocksServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
